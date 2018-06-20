4855ad4f16a9dd31ec3c70bb10cbb691bb4311dc
Thomas Callum Brook <thomascallumbrook@gmail.com>
2018-06-20 17:23:00 +0100

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+ module.exports = Phrase;
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
@@ -13,14 +14,21 @@ function emailSplit(email) {
 // Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+  }
+
   this.processor = function(string) {
     return string.toLowerCase();
   }
-
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.processor(this.letters());
   }
+
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
@@ -30,15 +38,3 @@ function Phrase(content) {
     return this.content.toUpperCase();
   };
 }
-
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-
-  // Returns translation processed for palindrome testing.
-  this.processedContent = function processedContent() {
-    return this.processor(this.translation);
-  }
-}
-
-TranslatedPhrase.prototype = new Phrase();
